e6887d6ef53ede7fc2d85cec4dd9fd1f75e6bb87
Chris D Brown <owner@chrisdbrown.co.uk>
2020-06-16 12:44:17 +0100

    feat: Announce messages in voice channel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv');
+const say = require('say');
 dotenv.config();
 
 const client = new Discord.Client();
@@ -7,21 +8,24 @@ const client = new Discord.Client();
 client.login(process.env.DISCORD_TOKEN);
 
 client.on('message', async (message) => {
-  console.log(message);
-
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
   if (!message.guild) return;
 
-  if (message.content === '/join') {
-    // Only try to join the sender's voice channel if they are in one themselves
+  if (message.content.startsWith('/say ')) {
     if (message.member.voice.channel) {
       const connection = await message.member.voice.channel.join();
 
-      const dispatcher = connection.play('/test.mp3');
+      const toSay = message.content.substr(5);
 
-      dispatcher.on('finish', () => {
-        console.log('Finished playing!');
+      say.export(toSay, 'Fiona', 1, 'output.wav', (err) => {
+        if (err) {
+          return console.error(err);
+        }
+
+        const dispatcher = connection.play('./output.wav');
+
+        dispatcher.on('finish', () => {
+          console.log('Finished playing!');
+        });
       });
     } else {
       message.reply('You need to join a voice channel first!');
